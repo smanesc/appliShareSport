2ecd1d9eae8ea854bdfbd27f24056181f5cf461c
Steph MANESCHI <stephanie.maneschi@hotmail.fr>
2022-08-19 14:14:56 +0200

    Add title bar and navbar

---
+++ src/app/tabs-account/tabs-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs-account',
+  templateUrl: './tabs-account.component.html',
+  styleUrls: ['./tabs-account.component.css']
+})
+export class TabsAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
